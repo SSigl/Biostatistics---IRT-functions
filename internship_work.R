1210e3179f3ac835897077fe66c221afb3460a90
SSigl <41140984+SSigl@users.noreply.github.com>
2018-08-13 11:28:58 +0200

    Update internship_work.R

--- internship_work.R
+++ internship_work.R
@@ -728,35 +728,35 @@ select_par = function(n){
 
 #===================================================================================#
 # importation of the dataset
-setwd("your_path")
-data <- read.csv("heiq.csv")
+#setwd("your_path")
+#data <- read.csv("heiq.csv")
 
 # creation and transformation of the databases
-study_1 <- subset(data,data$study==1)
-study_2 <- subset(data,data$study==2)
-study_3 <- subset(data,data$study==3)
-study_4 <- subset(data,data$study==4)
-study_5 <- subset(data,data$study==5)
+#study_1 <- subset(data,data$study==1)
+#study_2 <- subset(data,data$study==2)
+#study_3 <- subset(data,data$study==3)
+#study_4 <- subset(data,data$study==4)
+#study_5 <- subset(data,data$study==5)
 
 
-study_1 = select_not_na(study_1)
-study_2 = select_not_na(study_2)
-study_3 = select_not_na(study_3)
-study_4 = select_not_na(study_4)
-study_5 = select_not_na(study_5)
+#study_1 = select_not_na(study_1)
+#study_2 = select_not_na(study_2)
+#study_3 = select_not_na(study_3)
+#study_4 = select_not_na(study_4)
+#study_5 = select_not_na(study_5)
 
-datalist = list("study_1" = study_1,"study_2" =study_2,"study_3" =study_3,"study_4" =study_4,"study_5" =study_5)
-covariables = data.frame("study_1"=c("Sex","over64"),"study_2"=c("Sex","over48"),"study_3"=c("Sex","over72"),"study_4"=c("Sex","over76"),"study_5"=c("Sex","over51"))
+#datalist = list("study_1" = study_1,"study_2" =study_2,"study_3" =study_3,"study_4" =study_4,"study_5" =study_5)
+#covariables = data.frame("study_1"=c("Sex","over64"),"study_2"=c("Sex","over48"),"study_3"=c("Sex","over72"),"study_4"=c("Sex","over76"),"study_5"=c("Sex","over51"))
 
 # sub_scales
-healthBehaviour =paste("Heiq",c(1,9,13,19),sep="")
-engagementLife = paste("Heiq",c(2,5,8,10,15),sep="")
-emotionWellbeing = paste("Heiq",c(4,7,12,14,18,21),sep="")
-selfMonitoring = paste("Heiq",c(3,6,11,16,17,20),sep="")
-beingConstructive = paste("Heiq",c(27,34,36,39,40),sep="")
-skillAcquisition = paste("Heiq",c(23,25,26,30),sep="")
-socialIntegration = paste("Heiq",c(22,28,31,35,37),sep="")
-healthNavigation = paste("Heiq",c(24,29,32,33,38),sep="")
+#healthBehaviour =paste("Heiq",c(1,9,13,19),sep="")
+#engagementLife = paste("Heiq",c(2,5,8,10,15),sep="")
+#emotionWellbeing = paste("Heiq",c(4,7,12,14,18,21),sep="")
+#selfMonitoring = paste("Heiq",c(3,6,11,16,17,20),sep="")
+#beingConstructive = paste("Heiq",c(27,34,36,39,40),sep="")
+#skillAcquisition = paste("Heiq",c(23,25,26,30),sep="")
+#socialIntegration = paste("Heiq",c(22,28,31,35,37),sep="")
+#healthNavigation = paste("Heiq",c(24,29,32,33,38),sep="")
 
 
 
